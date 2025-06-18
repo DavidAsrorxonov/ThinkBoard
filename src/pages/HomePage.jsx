bd557ae01222f0f1d170547dafa63a818fd026c7
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2025-06-18 17:27:20 +0900

    Add NoteCard component and update HomePage to display notes in a grid layout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import RateLimitedUI from "../components/RateLimitedUI";
 import axios from "axios";
 import toast from "react-hot-toast";
+import NoteCard from "../components/NoteCard";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -37,12 +38,19 @@ const HomePage = () => {
 
       {isRateLimited && <RateLimitedUI />}
 
-      {notes.map((note) => (
-        <div key={note._id}>
-          <h2>{note.title}</h2>
-          <p>{note.content}</p>
-        </div>
-      ))}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading...</div>
+        )}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
